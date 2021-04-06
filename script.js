280c3931ae4d373e30098079f54078e8a906c117
Reka Palanki <reka.palanki@gmail.com>
2021-04-06 12:58:25 +0100

    adding click handler to all buttons

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hooray! It works!!!');
+// Change modal-outer classlist and add photo and description to modal-inner
+// Grab elements
+
+const modalOuter = document.querySelector('.modal-outer');
+const modalInner = document.querySelector('.modal-inner');
+const button = document.querySelectorAll('.card button');
+
+const clickHandler = (event) => {
+    modalOuter.classList.add('open');
+    modalInner.innerHTML = `
+    <p>It works!</p>
+    `
+}
+
+button.forEach(element => {
+    element.addEventListener('click', clickHandler);
+});
