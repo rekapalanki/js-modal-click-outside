e2ad5e2200e163cf2735812105a27a2421c048e1
Reka Palanki <reka.palanki@gmail.com>
2021-04-06 13:53:35 +0100

    adding handlers to close modal on clicking outside or Esc key

--- script.js
+++ script.js
@@ -6,17 +6,40 @@ const modalInner = document.querySelector('.modal-inner');
 const button = document.querySelectorAll('.card button');
 
 const clickHandler = (event) => {
+    event.stopPropagation();
     modalOuter.classList.add('open');
     // Add photo and description to modal-inner
     const card = event.target.closest('.card');
     const { src, alt } = card.querySelector('img');
     const description = card.dataset.description;
-    modalInner.innerHTML = `
+    const newInfoHTML = `
     <img src="${ src.replace("200", "400") }" alt="${ alt }">
     <p>${description}</p>
-    `
+    `;
+    modalInner.innerHTML = newInfoHTML;
 }
 
 button.forEach(element => {
     element.addEventListener('click', clickHandler);
 });
+
+// Close modal on clicking outside of the element
+
+const closeModalHandler = (event) => {
+    event.stopPropagation();
+    const outside = !event.target.closest('.modal-inner');
+    if (outside) {
+        modalOuter.classList.remove('open');
+    }
+}
+
+const escapeModalHandler = (event) => {
+    event.stopPropagation();
+    const escapeButton = (event.key === 'Escape');
+    if (escapeButton) {
+        modalOuter.classList.remove('open');
+    }
+}
+
+window.addEventListener('click', closeModalHandler);
+window.addEventListener('keydown', escapeModalHandler);
