d6aa1d4b4c41ad495ac7ad31442ac62abff8d6ae
Reka Palanki <reka.palanki@gmail.com>
2021-04-06 13:10:22 +0100

    adding photo and description to modal-inner

--- script.js
+++ script.js
@@ -7,8 +7,13 @@ const button = document.querySelectorAll('.card button');
 
 const clickHandler = (event) => {
     modalOuter.classList.add('open');
+    // Add photo and description to modal-inner
+    const card = event.target.closest('.card');
+    const { src, alt } = card.querySelector('img');
+    const description = card.dataset.description;
     modalInner.innerHTML = `
-    <p>It works!</p>
+    <img src="${ src.replace("200", "400") }" alt="${ alt }">
+    <p>${description}</p>
     `
 }
 
